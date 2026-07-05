8b4f896896fc56c17a4309811a0b6e0c0564c6c2
rafiqstar <mohdrafiqchowdhary786@gmail.com>
2026-07-05 07:15:20 +0530

    config.php
    Removed duplicate SECRET_KEY definition and adjusted formatting.

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
-<?php
+here<?php
 
-// Zaakpay Configuration
-
-define("API_KEY", "fa3801a07e2845b5b1ddce5f67665b37");
-define("SECRET_KEY", "09f0ff45caeb4cfcb220635549249c77");
+define("MERCHANT_IDENTIFIER", "c91ecf93993e4deda3959a7cd9db37d9");
+define("SECRET_KEY", "09f0ff45caeb4cfcb220635549249c77"); // Regenerated key use karein
 
 define("ZAAKPAY_URL", "https://api.zaakpay.com/api/paymentTransact/V8");
-
 define("RETURN_URL", "https://yourdomain.com/callback.php");
-
 define("CURRENCY", "INR");
-
-?>
