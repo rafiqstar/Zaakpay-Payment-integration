e6ae7898686b840ad4190de9cd9ac4f057b60108
rafiqstar <mohdrafiqchowdhary786@gmail.com>
2026-07-05 09:13:45 +0530

    Implement checksum generation in checkout.php
    Added checksum generation for payment processing.

--- checkout.php
+++ checkout.php
@@ -1,4 +1,15 @@
+here<?php
+
 $secret = "09f0ff45caeb4cfcb220635549249c77";
+
+$amount = $_POST['amount'];
+$email = $_POST['email'];
+$name = $_POST['name'];
+$lname = $_POST['lname'];
+$phone = $_POST['phone'];
+
+$orderId = "ORD" . time();
+$merchantId = "c91ecf93993e4deda3959a7cd9db37d9";
 
 $data = [
     "amount" => $amount,
@@ -10,7 +21,10 @@ $data = [
     "orderId" => $orderId
 ];
 
-// IMPORTANT: same order as Zaakpay docs
+// checksum generation (YAHI MAIN PART HAI)
 $hashString = implode("|", $data);
-
 $checksum = hash_hmac("sha256", $hashString, $secret);
+
+echo "Checksum: " . $checksum;
+
+?>
