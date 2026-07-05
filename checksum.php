a258b40a21800b57e9aa355dcc946efc15f23c19
rafiqstar <mohdrafiqchowdhary786@gmail.com>
2026-07-05 15:08:02 +0530

    Update checksum.php

--- checksum.php
+++ checksum.php
@@ -2,34 +2,56 @@
 
 define('SECRET_KEY', '09f0ff45caeb4cfcb220635549249c77');
 
-function getChecksumString($data)
+function calculateChecksum($data)
 {
-    $sequence = [
-        "amount","bankid","buyerAddress","buyerCity","buyerCountry",
-        "buyerEmail","buyerFirstName","buyerLastName","buyerPhoneNumber",
-        "buyerPincode","buyerState","currency","debitorcredit",
-        "merchantIdentifier","merchantIpAddress","mode","orderId",
-        "product1Description","product2Description","product3Description",
-        "product4Description","productDescription","productInfo","purpose",
-        "returnUrl","shipToAddress","shipToCity","shipToCountry",
-        "shipToFirstname","shipToLastname","shipToPhoneNumber",
-        "shipToPincode","shipToState","showMobile","txnDate",
-        "txnType","zpPayOption"
+    $fields = [
+        "amount",
+        "bankid",
+        "buyerAddress",
+        "buyerCity",
+        "buyerCountry",
+        "buyerEmail",
+        "buyerFirstName",
+        "buyerLastName",
+        "buyerPhoneNumber",
+        "buyerPincode",
+        "buyerState",
+        "currency",
+        "debitorcredit",
+        "merchantIdentifier",
+        "merchantIpAddress",
+        "mode",
+        "orderId",
+        "product1Description",
+        "product2Description",
+        "product3Description",
+        "product4Description",
+        "productDescription",
+        "productInfo",
+        "purpose",
+        "returnUrl",
+        "shipToAddress",
+        "shipToCity",
+        "shipToCountry",
+        "shipToFirstname",
+        "shipToLastname",
+        "shipToPhoneNumber",
+        "shipToPincode",
+        "shipToState",
+        "showMobile",
+        "txnDate",
+        "txnType",
+        "zpPayOption"
     ];
 
     $checksumString = "";
 
-    foreach ($sequence as $key) {
-        if (isset($data[$key]) && $data[$key] !== "") {
-            $checksumString .= $key . "=" . $data[$key] . "&";
-        }
+    foreach ($fields as $field) {
+        $value = isset($data[$field]) ? $data[$field] : "";
+        $checksumString .= $field . "=" . $value . "&";
     }
 
-    return rtrim($checksumString, "&");
-}
+    $checksumString = rtrim($checksumString, "&");
 
-function calculateChecksum($data)
-{
-    $checksumString = getChecksumString($data);
     return hash_hmac("sha256", $checksumString, SECRET_KEY);
 }
