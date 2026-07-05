6c0a66b5391862d13f00156d6baecd081b00fa9e
rafiqstar <mohdrafiqchowdhary786@gmail.com>
2026-07-05 06:46:36 +0530

    API page

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Zaakpay Configuration
+
+define("API_KEY", "fa3801a07e2845b5b1ddce5f67665b37");
+define("SECRET_KEY", "09f0ff45caeb4cfcb220635549249c77");
+
+define("ZAAKPAY_URL", "https://api.zaakpay.com/api/paymentTransact/V8");
+
+define("RETURN_URL", "https://yourdomain.com/callback.php");
+
+define("CURRENCY", "INR");
+
+?>
